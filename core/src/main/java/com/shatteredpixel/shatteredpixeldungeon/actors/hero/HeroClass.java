3aa7785d5be67859a991857842726d405275542c
BorgField <147969784+BorgField@users.noreply.github.com>
2025-10-15 17:58:27 +0800

    初始化

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
@@ -127,7 +127,10 @@ public enum HeroClass {
 	HUNTRESS( HeroSubClass.SNIPER, HeroSubClass.WARDEN ),
 
 	//New Hero-Radish Pixel Dungeon
-	RECTOR( HeroSubClass.BATTLEPREIST, HeroSubClass.REDCARDINAL, HeroSubClass.DEAD_KNIGHT);
+	RECTOR( HeroSubClass.BATTLEPREIST, HeroSubClass.REDCARDINAL, HeroSubClass.DEAD_KNIGHT),
+	
+	//New Hero-Moonlight-Radish Pixel Dungeon
+	MOONLIGHT( HeroSubClass.BLADESHIELD_KNIGHT, HeroSubClass.LITTLE_KNIGHT);
 
 //	DUELIST( HeroSubClass.CHAMPION, HeroSubClass.MONK );
 
@@ -263,6 +266,10 @@ public enum HeroClass {
 			case RECTOR:
 				initRector( hero );
 				break;
+
+			case MOONLIGHT:
+				initMoonlight( hero );
+				break;
 		}
 
 		if (SPDSettings.quickslotWaterskin()) {
@@ -345,6 +352,24 @@ public enum HeroClass {
 		new ScrollOfIdentify().identify();
 		new PotionOfExperience().identify();
 		new ScrollOfRemoveCurse().identify();
+	}
+
+	private static void initMoonlight( Hero hero ) {
+
+		(hero.belongings.weapon = new WornShortsword()).identify();
+
+		WandOfMagicMissile wand = new WandOfMagicMissile();
+		wand.quantity(1).identify().collect();
+		Dagger dagger = new Dagger();
+		dagger.quantity(1).identify().collect();
+		Gloves gloves = new Gloves();
+		gloves.quantity(1).identify().collect();
+		MetalCross mata = new MetalCross();
+		mata.quantity(1).identify().collect();
+
+		new PotionOfHealing().identify();
+		new ScrollOfUpgrade().identify();
+		new ScrollOfIdentify().identify();
 	}
 
 	public String title() {
