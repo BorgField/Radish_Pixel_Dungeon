35e81e269ff29c0b2317a15ca7361f7538bc3cb1
BorgField <147969784+BorgField@users.noreply.github.com>
2026-03-07 20:17:23 +0800

    合并

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/hero/HeroClass.java
@@ -30,10 +30,6 @@ import com.shatteredpixel.shatteredpixeldungeon.SPDSettings;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.rector.Belief;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.ArmorAbility;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.Ratmogrify;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.duelist.Challenge;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.duelist.ElementalStrike;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.duelist.Feint;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.huntress.NaturesPower;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.huntress.SpectralBlades;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.abilities.huntress.SpiritHawk;
@@ -436,8 +432,6 @@ public enum HeroClass {
 
 	public String splashArt(){
 		switch (this) {
-			case WARRIOR: default:
-				return Assets.Splashes.WARRIOR;
 			case MAGE:
 				return Assets.Splashes.MAGE;
 			case ROGUE:
@@ -448,6 +442,8 @@ public enum HeroClass {
 				return Assets.Splashes.RECTOR;
 			case MOONLIGHT:
 				return Assets.Splashes.MOONLIGHT;
+			case WARRIOR: default:
+				return Assets.Splashes.WARRIOR;
 		}
 	}
 	
